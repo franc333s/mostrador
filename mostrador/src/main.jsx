da1b87179d873bacb78b4200dbda22f1a5559db6
franc333s <marinafase@gmail.com>
2023-11-08 13:42:40 +0100

    test out gitignore

--- mostrador/src/main.jsx
+++ mostrador/src/main.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
+import { FirebaseAppProvider } from 'reactfire';
 
+const config = {
+   // test about config
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <FirebaseAppProvider firebaseConfig={config}>
+                <App />
+            </FirebaseAppProvider>
         </BrowserRouter>
     </React.StrictMode>,
 )
